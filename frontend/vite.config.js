93776845688d425189b2f08b1c94b2dbbc865da4
KeonSuYun <904085736@qq.com>
2026-01-02 04:51:29 +0800

    v4.6 尝试修复客户端

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // <--- 🔴 必须加上这一行！将绝对路径改为相对路径
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
